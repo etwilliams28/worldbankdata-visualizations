b725d2879030b04b1985625224ee3334ec0650f7
Williams <ethanwi@carriere.ca>
2021-01-25 12:31:04 -0500

    first commit

---
+++ worldbankapp/routes.py
@@ -0,0 +1,16 @@
+from worldbankapp import app 
+import json, plotly
+from flask import render_template
+from wrangling_scripts.wrangle_data import return_figures
+
+@app.route('/')
+@app.route('/index')
+def index():
+    figures = return_figures()
+
+    #plot id for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index', ids=ids, figuresJSON=figuresJSON)
